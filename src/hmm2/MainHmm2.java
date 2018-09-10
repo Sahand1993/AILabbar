21efdc74ab04b9b0cb7a9a1932d2a7686b0a7169
Sahand1993 <sahandz@hotmail.com>
2018-09-10 08:36:01 +0200

    almost finished, backtracking in deltaIndex left.

---
+++ src/hmm2/MainHmm2.java
@@ -0,0 +1,102 @@
+package hmm2;
+
+import hmm1.MainHmm1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MainHmm2 extends MainHmm1 {
+
+    static float[][] delta;
+    static int[][] deltaIndex;
+
+    public static void main(String[] args){
+        br = new BufferedReader(new InputStreamReader(System.in));
+        readInput();
+
+        delta = new float[A.length][O.length];
+        deltaIndex = new int[A.length][O.length];
+
+        fillDelta();
+
+    }
+
+    /**
+     * Fills delta and deltaIndex matrices with probabilities.
+     */
+    public static void fillDelta(){
+        firstColDelta();
+
+        float[] probs;
+        // For each timestep t
+        for(int t = 1; t < O.length; t++) {
+            // For each possible state at t
+            for(int i = 0; i < A.length; i++) {
+                probs = new float[A.length];
+                // For each possible state at t - 1
+                for(int j = 0; j < A.length; j++) {
+                    probs[j] = delta[j][t-1] * A[j][i] * B[i][O[t]];
+                }
+                FloatInt floatInt = max(probs);
+                delta[i][t] = floatInt.getMax();
+                deltaIndex[i][t] = floatInt.getArgmax();
+            }
+        }
+    }
+
+    /**
+     *  Fills the first column of delta
+     */
+    public static void firstColDelta(){
+        for(int i = 0; i < A.length; i++) {
+            delta[0][i] = pi[0][i] * B[i][O[0]];
+        }
+    }
+
+    /**
+     * Return max of array
+     */
+    public static FloatInt max(float[] arr){
+        float max = arr[0];
+        int argmax = 0;
+        for(int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                argmax = i;
+            }
+        }
+        return new FloatInt(max, argmax);
+    }
+
+    /**
+     * Finds the most likely path by backtracking in deltaIndex.
+     */
+    public void findSequence(){
+
+    }
+
+    private static class FloatInt {
+        private float max;
+        private int argmax;
+        public FloatInt(float max, int argmax) {
+            this.max = max;
+            this.argmax = argmax;
+        }
+
+        public float getMax() {
+            return max;
+        }
+
+        public void setMax(float max) {
+            this.max = max;
+        }
+
+        public int getArgmax() {
+            return argmax;
+        }
+
+        public void setArgmax(int argmax) {
+            this.argmax = argmax;
+        }
+    }
+}
---
+++ src/hmm2/MainHmm2Test.java
@@ -0,0 +1,40 @@
+package hmm2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainHmm2Test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+        MainHmm2 main = new MainHmm2();
+
+        String fileName = "src/hmm2/samples/hmm3_01.in";
+
+        FileInputStream is = null;
+
+        try {
+            is = new FileInputStream(fileName);
+            System.setIn(is);
+            main.main(new String[] {});
+            main.printMatrix(main.delta);
+            main.printMatrix(main.deltaIndex);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
