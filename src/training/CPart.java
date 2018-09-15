3654919e678ca5735907c810a77af7ed77b7a31e
Sahand1993 <sahandz@hotmail.com>
2018-09-15 14:55:21 +0200

    created skeleton

---
+++ src/training/CPart.java
@@ -0,0 +1,23 @@
+package training;
+
+public class CPart {
+    public static void main(String[] args) {
+        question7();
+        question8();
+        question9();
+        question10();
+    }
+
+    private static void question7() {
+    }
+
+    private static void question8() {
+
+    }
+
+    private static void question9() {
+    }
+
+    private static void question10() {
+    }
+}
